29dba08e525dc544ef4adf87770a7d780e758448
Mateo M.H <mateomh48@gmail.com>
2022-09-01 01:29:54 -0500

    Change Rofi Theme and Add Kitty to Dotfiles

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -59,6 +59,10 @@ keys = [Key(key[0], key[1], *key[2:]) for key in [
     # Terminal
     ([mod], "Return", lazy.spawn("alacritty")),
 
+    # Kitty Terminal
+    ([mod, "shift"], "Return", lazy.spawn("kitty")),
+
+
     # Redshift
     ([mod], "r", lazy.spawn("redshift -O 2400")),
     ([mod, "shift"], "r", lazy.spawn("redshift -x")),
